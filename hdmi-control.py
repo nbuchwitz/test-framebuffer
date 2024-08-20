9d1c4ab1d8dbe9e39fb2a69728c53dd0e4533bcb
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2024-08-20 11:26:58 +0200

    feat: Support 16-bit depth framebuffer
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- hdmi-control.py
+++ hdmi-control.py
@@ -9,7 +9,11 @@ KD_GRAPHICS = 0x01
 FBIOGET_VSCREENINFO = 0x4600
 
 # colors defined in BGRA (right to left) format
-_colors = {"red": 0xFFFF0000, "green": 0xFF00FF00, "blue": 0xFF0000FF}
+_colors_32 = {"red": 0xFFFF0000, "green": 0xFF00FF00, "blue": 0xFF0000FF}
+# colors for 16-bit depth (RGB565)
+_colors_16 = {"red": 0xF800, "green": 0x07E0, "blue": 0x001F}
+
+assert _colors_16.keys() == _colors_32.keys()
 
 
 def tty_set_mode(tty_name: str, kd_mode: int) -> None:
@@ -32,19 +36,28 @@ def fill_framebuffer_with_color(color: str, framebuffer: int = 0) -> None:
     """Fill framebuffer with one color."""
     fb = os.open(f"/dev/fb{framebuffer}", os.O_RDWR)
 
-    # Get the screen size
+    # Get info about framebuffer (size, bpp, ...)
     screen_info = struct.unpack(
-        "HHHH", fcntl.ioctl(fb, FBIOGET_VSCREENINFO, struct.pack("HHHH", 0, 0, 0, 0))
+        "8I", fcntl.ioctl(fb, FBIOGET_VSCREENINFO, b"\x00" * 32)
     )
-    screen_width, screen_height = screen_info[0], screen_info[2]
+    screen_width, screen_height = screen_info[0], screen_info[1]
+    bits_per_pixel = screen_info[6]
     screen_size = screen_width * screen_height
 
-    color_value = _colors.get(color, None)
+    if bits_per_pixel == 32:
+        color_value = _colors_32.get(color, None)
+        pack_format = "I"  # 32-bit unsigned int
+    elif bits_per_pixel == 16:
+        color_value = _colors_16.get(color, None)
+        pack_format = "H"  # 16-bit unsigned short
+    else:
+        raise ValueError(f"Unsupported bits per pixel: {bits_per_pixel}")
+
     if color_value is None:
         raise ValueError("Invalid color specified.")
 
     # Fill the framebuffer with the specified color
-    data = struct.pack("I", color_value) * screen_size
+    data = struct.pack(pack_format, color_value) * screen_size
     os.write(fb, data)
 
     os.close(fb)
@@ -55,10 +68,12 @@ if __name__ == "__main__":
         print("insufficient permission: script needs root permissions.")
         exit(1)
 
-    known_colors = list(_colors.keys())
+    known_colors = list(_colors_16.keys())
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--color", type=str, choices=known_colors, default=known_colors[0])
+    parser.add_argument(
+        "--color", type=str, choices=known_colors, default=known_colors[0]
+    )
     parser.add_argument("--mode", type=str, choices=["color", "tty"], required=True)
     parser.add_argument("--tty", type=str, required=False, default="/dev/tty0")
 
